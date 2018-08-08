5697866ff5de42f24db55f252776eedad5e684e8
Eli Cho <eli.cho@gmail.com>
2018-08-08 21:09:13 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,8 @@ class Anagram
     @word = word
   end
 
-  def match(word_array)
-    word_array.select do |word|
+  def match(arr)
+    arr.select do |word|
       word.split("").sort == @word.split("").sort
     end
   end
